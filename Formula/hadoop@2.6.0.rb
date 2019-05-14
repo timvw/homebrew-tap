bdd01d6cee9ddf37bf50824e8ad4b4daaf02fe58
Tim Van Wassenhove <tim@timvw.be>
2019-05-14 15:04:06 +0200

    initial commit

---
+++ Formula/hadoop@2.6.0.rb
@@ -0,0 +1,25 @@
+class HadoopAT260 < Formula
+  desc "Framework for distributed processing of large data sets"
+  homepage "https://hadoop.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  conflicts_with "yarn", :because => "both install `yarn` binaries"
+
+  def install
+    rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
+    libexec.install %w[bin sbin libexec share etc]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    sbin.write_exec_script Dir["#{libexec}/sbin/*"]
+    libexec.write_exec_script Dir["#{libexec}/libexec/*.sh"]
+    # Temporary fix until https://github.com/Homebrew/brew/pull/4512 is fixed
+    chmod 0755, Dir["#{libexec}/*.sh"]
+  end
+
+  test do
+    system bin/"hadoop", "fs", "-ls"
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Timvw tap
+
+## How do I install these formulae?
+`brew install timvw/tap<formula>`
+
+Or `brew tap timvw/tap` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/timvw/homebrew-tap/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
