0ffe51d6685ffbcdd40e2e46ab405a358fd9d8e2
Tim Van Wassenhove <github@timvw.be>
2022-06-02 18:09:30 +0000

    arrow-flight-sql-client 0.4.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/arrow-flight-sql-client.rb
+++ Formula/arrow-flight-sql-client.rb
@@ -3,8 +3,8 @@
 class ArrowFlightSqlClient < Formula
   desc "Arrow Flight SQL Client"
   homepage "https://github.com/timvw/arrow-flight-sql-client"
-  url "https://github.com/timvw/arrow-flight-sql-client/releases/download/v0.3.0/arrow-flight-sql-client-0.3.0-x86_64-apple-darwin-generic.tar.gz"
-  sha256 "1f3039d24ee9989c80e81b1566d93005b8c76d1534236ac4f7a375c88ce35106"
+  url "https://github.com/timvw/arrow-flight-sql-client/releases/download/v0.4.0/arrow-flight-sql-client-0.4.0-x86_64-apple-darwin-generic.tar.gz"
+  sha256 "dc57a7ba0e7ca59cd0a393c8e52c88d61d21ae55729e07557845ce86ee1fe706"
   license "Apache-2.0"
 
   def install
