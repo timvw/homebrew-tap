398ee5d6793833dd8ae8542a2e557bbe84872cb1
Tim Van Wassenhove <tim@timvw.be>
2022-06-10 23:06:37 +0200

    bump to  0.1.21

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -3,8 +3,7 @@
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  url "https://github.com/timvw/qv/releases/download/0.1.16/qv-0.1.16-x86_64-apple-darwin-generic.tar.gz"
-  sha256 "f08c56cbdac3cdb8785df8bcd434d863186dd9975bfbde751999a4d4fe29c60d"
+  url "https://github.com/timvw/qv/releases/download/0.1.21/qv-0.1.21-x86_64-apple-darwin-generic.tar.gz"
   license "Apache-2.0"
 
   def install
