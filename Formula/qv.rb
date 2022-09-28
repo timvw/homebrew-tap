e0423dec10da5ba18288d12bd1484f2fe5aed115
Tim Van Wassenhove <github@timvw.be>
2022-09-28 10:40:59 +0200

    qv 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -3,7 +3,7 @@
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  url "https://github.com/timvw/qv/releases/download/v0.2.0/qv-0.2.0-x86_64-apple-darwin-generic.tar.gz"
+  url "https://github.com/timvw/qv/releases/download/v0.3.0/qv-0.3.0-x86_64-apple-darwin-generic.tar.gz"
   license "Apache-2.0"
 
   def install
