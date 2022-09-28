ac89f89d268bb273e496e3988e4c9f12ab1859ff
Tim Van Wassenhove <tim@timvw.be>
2022-09-28 15:08:14 +0200

    correct desc

--- Formula/arrow-flight-sql-client.rb
+++ Formula/arrow-flight-sql-client.rb
@@ -1,5 +1,5 @@
 class ArrowFlightSqlClient < Formula
-  desc "Arrow Flight SQL Client"
+  desc "Client to quer Arrow Flight SQL Servers"
   homepage "https://github.com/timvw/arrow-flight-sql-client"
   url "https://github.com/timvw/arrow-flight-sql-client/releases/download/v0.4.0/arrow-flight-sql-client-0.4.0-x86_64-apple-darwin-generic.tar.gz"
   sha256 "dc57a7ba0e7ca59cd0a393c8e52c88d61d21ae55729e07557845ce86ee1fe706"
