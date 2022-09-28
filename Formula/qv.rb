64863f48d3e49f08a5bcf9d81bdef87f4904d1ed
Tim Van Wassenhove <github@timvw.be>
2022-09-28 21:50:44 +0200

    qv 0.3.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -1,8 +1,8 @@
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  url "https://github.com/timvw/qv/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "6278a2b43d05da21d3625a69b1419c020bb706be8f83710ea832b5d42b254c91"
+  url "https://github.com/timvw/qv/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "5a22c7f1ceaabebf8e3ae55a7958474a4821a038e74a2a7b1f8de520c7d5b242"
   license "Apache-2.0"
   head "https://github.com/timvw/qv.git", branch: "main"
 
