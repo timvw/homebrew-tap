b25e212ee234c092c800c23e58a0547e1ca8cdbc
Tim Van Wassenhove <tim@timvw.be>
2022-09-28 14:12:49 +0200

    updated formula for qv

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -1,12 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
+
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  url "https://github.com/timvw/qv/releases/download/v0.3.1/qv-0.3.1-x86_64-apple-darwin-generic.tar.gz"
+  url "https://github.com/timvw/qv/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "9ad50c79a90ecab399d861429ff045309c9d7e45012e9dd930e313c61bf4b9fe"
   license "Apache-2.0"
+  head "https://github.com/timvw/qv.git", branch: "main"
+
+  depends_on "rust" => :build
 
   def install
-    bin.install "qv"
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    shell_output("#{bin}/qv")
   end
 end
+
