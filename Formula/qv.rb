0b49b0b924ab5a80fe8ccefee49b2f81de0b007f
Tim Van Wassenhove <github@timvw.be>
2024-03-30 15:32:38 +0000

    qv 0.9.3
    Closes #42.
    
    Signed-off-by: Tim Van Wassenhove <github@timvw.be>

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -1,8 +1,8 @@
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  url "https://github.com/timvw/qv/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "d6c9260d7e9cfb87e5ef20f70766b156a653dfd97ace1c6ad7da53508fc13443"
+  url "https://github.com/timvw/qv/archive/refs/tags/v0.9.3.tar.gz"
+  sha256 "1a9625265bd94f65645c4d5e578f541f9635d224032344125032bf7082baed23"
   license "Apache-2.0"
   head "https://github.com/timvw/qv.git", branch: "main"
 
