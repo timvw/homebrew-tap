95e83c8eece7e255333bb3c024770db701d5a2ec
Tim Van Wassenhove <github@timvw.be>
2025-11-22 14:09:52 +0100

    chore: limit bose formula to macos

--- Formula/bose.rb
+++ Formula/bose.rb
@@ -4,6 +4,7 @@ class Bose < Formula
   url "https://github.com/timvw/bose/releases/download/v0.2.0/bose-0.2.0-macos-universal.tar.gz"
   sha256 "d0616decd3aca56e0b8642d6b26fd955288c6c86f70d5847fcaac59f25f6c3f4"
   license "Apache-2.0"
+  depends_on :macos
 
   def install
     bin.install "bose-universal-macos" => "bose"
