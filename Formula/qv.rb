2f3b75b8236636fbd7911f0eba9ff85f16f7f255
Tim Van Wassenhove <github@timvw.be>
2025-11-25 07:16:37 +0100

    Use CPU conditional for qv artifacts

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -1,11 +1,14 @@
 class Qv < Formula
   desc "Quickly view your data"
   homepage "https://github.com/timvw/qv"
-  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/timvw/qv/releases/download/v0.9.14/qv-#{arch}-apple-darwin.tar.gz"
-  sha256 arm: "663ce40c3a4e68a6b4feef93d919534c22922cc352588129c49e816a00359b4d",
-         intel: "26a19254960b934a9430f3830df64400de98f25e2a669b1e3bfad09a32176f76"
+  if Hardware::CPU.arm?
+    url "https://github.com/timvw/qv/releases/download/v0.9.14/qv-aarch64-apple-darwin.tar.gz"
+    sha256 "663ce40c3a4e68a6b4feef93d919534c22922cc352588129c49e816a00359b4d"
+  else
+    url "https://github.com/timvw/qv/releases/download/v0.9.14/qv-x86_64-apple-darwin.tar.gz"
+    sha256 "26a19254960b934a9430f3830df64400de98f25e2a669b1e3bfad09a32176f76"
+  end
   license "Apache-2.0"
   head "https://github.com/timvw/qv.git", branch: "main"
 
