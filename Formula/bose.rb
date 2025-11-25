3092c31ca8b208dd1a49695d75abd49e8a7dafba
Tim Van Wassenhove <github@timvw.be>
2025-11-25 14:57:43 +0100

    Revert manual bose formula bump

--- Formula/bose.rb
+++ Formula/bose.rb
@@ -1,8 +1,8 @@
 class Bose < Formula
   desc "CLI to control Bose SoundTouch speakers"
   homepage "https://github.com/timvw/bose"
-  url "https://github.com/timvw/bose/releases/download/v0.2.10/bose-v0.2.10-apple-darwin-universal.tar.gz"
-  sha256 "f378deddc0bc8de70b3f91dfdd83a6becba782f0b689ad3290d6b19eb2138724"
+  url "https://github.com/timvw/bose/releases/download/v0.2.3/bose-v0.2.3-apple-darwin-universal.tar.gz"
+  sha256 "6493d532efcb061c5d0a8ec1dbacdfe76ba931c3da94b872e4fd2363f4827ef8"
   license "Apache-2.0"
   # macOS-only binary; Linux checks only validate artifact availability.
   depends_on :macos
