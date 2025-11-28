7a1a27106f1398c4fef5e393d9d9a138282fd829
Tim Van Wassenhove <github@timvw.be>
2025-11-28 17:10:38 +0100

    fix: resolve merge conflict and update wt to v0.1.4 with correct bottles

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -7,11 +7,11 @@ class Wt < Formula
   head "https://github.com/timvw/wt.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/timvw/wt/releases/download/v0.1.3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fefbc90d6c0514f60ce59914cdf09a904e8a6585563e1c1bae356b38075c39fe"
-    sha256 cellar: :any_skip_relocation, ventura:       "6e4c53175dc6a5cc5cfce95d0a4b4c50b29e9a48fa29f56a8a7b13352948863f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "101b699b71e57eb10d859f83b0381902e37700dd64624e1c288c4459158e27c6"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "626ffaa7c82943280a493e1dd4716a2fc8ef970ae46f9f79298c28663578e952"
+    root_url "https://github.com/timvw/wt/releases/download/v0.1.4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5fd336d8f7e6d5c2e598dae0edffbaaef53b212739b235adbc6e656e034b39cf"
+    sha256 cellar: :any_skip_relocation, ventura:       "c8aa5035fcb1d6dd5e8ea8794bcf0df309dd9f009283cca71c223f9cb8b2b97c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8b38b0a3076c19af5988075cc74d30519092c5e6f32e0b16dd0d30f15e0b0a3"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "80f3c867600b05a04404405b22cbe103991d07421559b49b1ddc363c3e1f6814"
   end
 
   def install
